6034c8f174358b161a144ac320b341377c485fd7
caroline1233456 <caroline-qingru.yang@outlook.com>
2025-01-19 17:01:36 +0800

    level 2 completed

--- src/main/java/Parakeet.java
+++ src/main/java/Parakeet.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Parakeet {
+
     public static void main(String[] args) {
+        ArrayList list = new ArrayList<String>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Parakeet");
         System.out.println("What can I do for you?");
@@ -15,8 +18,16 @@ public class Parakeet {
                 System.out.println("____________________________________________________________");
                 break;
             }
+            if (command.equalsIgnoreCase("list")){
+                System.out.println("____________________________________________________________");
+                for(int i=0; i<list.size();i++){
+                    System.out.println(i+1+"."+list.get(i).toString());
+                }
+                System.out.println("____________________________________________________________");
+            }
             System.out.println("____________________________________________________________");
             System.out.println(command);
+            list.add(command);
             System.out.println("____________________________________________________________");
 
         }

b24db57d895124fa362787b4856c33be883768f7
caroline1233456 <caroline-qingru.yang@outlook.com>
2025-01-19 16:46:08 +0800

    level 1 completed

--- src/main/java/Parakeet.java
+++ src/main/java/Parakeet.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
 public class Parakeet {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Parakeet");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(command);
+
+        }
+        scanner.close();
 
     }
 }
